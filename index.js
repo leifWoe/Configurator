c57a7ce793df1d1674eaba8c133604d0bd64f871
leifWoe <woehlk.lennart@gmail.com>
2023-05-31 14:54:36 +0200

    logic for uplinks and rsp

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //! highlight choosen option and add value to output table
 //! SIZES aka GRÖßEN
+
 const sizeOutputTable = document.getElementById('sizeOutputTable');
-const sizeOptions = document.querySelectorAll('.sizeOptions');
 const sizeDefaultTd = document.getElementById('sizeDefaultTd');
-let currentSizeTd = null;
+const sizeOptions = document.querySelectorAll('.sizeOptions');
+let sizeCurrentTd = null;
 
 sizeOptions.forEach(function(element) {
     element.addEventListener('click', function(e) {
@@ -15,44 +16,86 @@ sizeOptions.forEach(function(element) {
 
         sizeDefaultTd.textContent = this.textContent + ' Rack';
 
-        if (currentSizeTd) {
-            currentSizeTd.replaceWith(sizeDefaultTd);
-        }
-        else {
-            const newRow = outputTable.insertRow();
-            newRow.appendChild(sizeDefaultTd);
-        }
-        currentSizeTd = sizeDefaultTd;
+    if (sizeCurrentTd) {
+        sizeCurrentTd.replaceWith(sizeDefaultTd);
+    }
+    else {
+        const newRow = outputTable.insertRow();
+        newRow.appendChild(sizeDefaultTd);
+    }
+    sizeCurrentTd = sizeDefaultTd;
     });
 });
 
+
 //! LOCATION aka ORT
+
 const locationOutputTable = document.getElementById('locationOutputTable')
-const locationOptions = document.querySelectorAll('.locationOptions');
 const locationDefaultTd = document.getElementById('locationDefaultTd');
-let currentLocationTd = null;
+const locationOptions = document.querySelectorAll('.locationOptions');
+let locationCurrentTd = null;
 
 locationOptions.forEach(function(element) {
     element.addEventListener('click', function(e) {
 
+        // show choosen Option 
         locationOptions.forEach(function(element) {
             element.classList.remove('active');
         });
         this.classList.add('active');
 
         locationDefaultTd.textContent = this.textContent;
-
-        if (currentLocationTd) {
-            currentLocationTd.replaceWith(locationDefaultTd);
-        }
-        else {
-            const newRow = locationOutputTable.insertRow();
-            newRow.appendChild(locationDefaultTd);
-        }
-        currentLocationTd = locationDefaultTd;
+    
+    //
+    if (locationCurrentTd) {
+        locationCurrentTd.replaceWith(locationDefaultTd);
+    }
+    else {
+        const newRow = locationOutputTable.insertRow();
+        newRow.appendChild(locationDefaultTd);
+    }
+    locationCurrentTd = locationDefaultTd;
     });
 });
 
+
 //! UPLINKS
-const uplinksOutputTable = document.getElementById('uplinksOutputTable');
-const  = ;
+window.addEventListener("DOMContentLoaded", () => {
+    const uplinksDefaultTd = document.getElementById('uplinksDefaultTd');
+    const uplinksPlusOption = document.getElementById('uplinksPlusOption');
+    const uplinksMinusOption = document.getElementById('uplinksMinusOption');
+    let uplinksCount = 1;
+
+    const uplinksCounterUpdate = () => {
+        uplinksDefaultTd.textContent = uplinksCount + ' Uplink(s)'
+        };
+
+    if (uplinksPlusOption) {
+        uplinksPlusOption.addEventListener('click', () => {
+            uplinksCount++;
+            uplinksCounterUpdate()
+        })};
+
+    if (uplinksMinusOption) {
+        uplinksMinusOption.addEventListener('click', () => {
+            uplinksCount--;
+            uplinksCounterUpdate()
+        })};
+});
+
+//! REMOTEHAND SERVICE PAKET
+window.addEventListener("DOMContentLoaded", () => {
+    const rspDefaultTd = document.getElementById('rspDefaultTd');
+    const rspNoOption = document.getElementById('rspNoOption');
+    const rspYesOption = document.getElementById('rspYesOption');
+
+    if (rspYesOption) {
+        rspYesOption.addEventListener('click', () => {
+            rspDefaultTd.textContent = 'ohne Remote Service Paket';
+    })};
+
+    if (rspNoOption) {
+        rspNoOption.addEventListener('click', () => {
+            rspDefaultTd.textContent = 'mit Remote Service Paket';
+    })};
+});
