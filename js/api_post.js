74039c2cbe3eaf6fd0f9b1a8c4a1a0ec6396b6fd
leifWoe <woehlk.lennart@gmail.com>
2023-06-20 17:20:09 +0200

    updated post script with zammad docu

--- js/api_post.js
+++ js/api_post.js
@@ -1,42 +1,44 @@
 const formElement = document.querySelector('#sendForm');
 const REQUEST = 'https://reqres.in/api/unknown'
+const API_TOKEN = 
+// todo bash: curl -H "Authorization: Token YOUR_API_TOKEN" https://your-zammad-instance/api/v1/your-endpoint
+//? Replace "YOUR_API_TOKEN" with your actual API token and "https://your-zammad-instance/api/v1/your-endpoint" with the appropriate API endpoint you want to access.
 //? https://zammad/api/v1/tickets
-//? https://docs.zammad.org/en/latest/api/intro.html?highlight=api#endpoints-and-example-data
-//? https://docs.zammad.org/en/latest/api/ticket/index.html
+//? https://docs.zammad.org/en/latest/api/intro.html#authentication
 
 
 formElement.addEventListener('submit', event => {
   event.preventDefault();
   var formData = new FormData(formElement);
-  //console.log(formData.get('sizeSendInfo')); //todo: set / append / delete
+  //console.log(formData.get('sizeSendInfo')); //? set / append / delete
 
 
-  var DATUM = "01.01.2001";
+  var DATUM = "false Datum -> use created at";
   var DATA = [];
-  for (const [v, value] of formData) {
+  for (const [key, value] of formData) {
     DATA.push({key: value});
   }
   
+//? make status http code? 
 
   var REQUEST_BODY = {
     "title": "test ticket",
     "group": "User inquiry",
+    "X-On-Behalf-Of": "user ID/ user login/ user email", //? or as "customer"?
     "article": {
-        "subject": "Rack Anfrage vom"+ DATUM, // todo: use created at
-        "body": DATA,
-        "type": "X",
-        "internal": true,
+      "subject": "Rack Anfrage vom"+ DATUM, // todo: use created at
+      "body": DATA,
+      "type": "inquiry",
+      "internal": false,
     },
-    "customer": "email@user.com",
-    "user": "email@user.com",
-    "note": "X"
-    }
+  }
   
-
+  
   fetch(REQUEST, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': API_TOKEN
     },
     body: JSON.stringify(REQUEST_BODY),
   })
--- js/get.js
+++ js/get.js
@@ -1,17 +1,17 @@
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'your_database_name'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'your_username',
+//     password: 'your_password',
+//     database: 'your_database_name'
+// });
 
-connection.query('SELECT * FROM ', (error, results) => {
-    if (error) {
-        throw error;
-    }
+// connection.query('SELECT * FROM ', (error, results) => {
+//     if (error) {
+//         throw error;
+//     }
 
 
-// todo add the data from sql to options as values
+// // todo: add the data from sql to options as values
 
-connection.end();
-});
+// connection.end();
+// });
