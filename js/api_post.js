c48d597bb40d833d5d6a23388f023c9bdc3fd0aa
leifWoe <woehlk.lennart@gmail.com>
2023-07-11 14:20:49 +0200

    changed structure of fetch

--- js/api_post.js
+++ js/api_post.js
@@ -2,9 +2,6 @@ const formElement = document.querySelector('#sendForm');
 const REQUEST_URL = 'https://support.speedbone.de/api/v1/tickets'
 const API_TOKEN = 'YHkpseDxJtYZ8ohGpa4EzwJjt9jNOFnZ2U5eshp9HEwoG9kxX_ooO4hjJM8ZzVo6'
 //! docu:
-// keine Ahnung was das fürn token ist: gSQGQq1HBAVzAKVYHIPu-gpL03V4U0AG1CogzpaaZ8uDcZGeeUsUSipn0MXQexDg
-
-// alles token: h_sHaMvYVfPRRJ_LdgW3F3018xsh44Iy2iox6onvIbhsazdQIRBODWgiDBeB_30E
 // only agent api token: YHkpseDxJtYZ8ohGpa4EzwJjt9jNOFnZ2U5eshp9HEwoG9kxX_ooO4hjJM8ZzVo6
 
 // https://support.speedbone.de/api/v1/groups/1 work ???
@@ -17,22 +14,8 @@ const API_TOKEN = 'YHkpseDxJtYZ8ohGpa4EzwJjt9jNOFnZ2U5eshp9HEwoG9kxX_ooO4hjJM8Zz
 formElement.addEventListener('submit', event => {
   event.preventDefault();
 
-
-  var API_HEADERS = new Headers();
-
-  API_HEADERS.append(
-    'Authorization', 'Token token=' + API_TOKEN);
-  API_HEADERS.append(
-    'Content-Type', 'application/json');
-
-  var REQUEST_OPTIONS = {
-    method: 'POST',
-    headers: API_HEADERS,
-  };
-
   
-  var formData = new FormData(formElement);
-  //? formData.get('sizeSendInfo'); set / append / delete -> could change input after user input
+  var formData = new FormData(formElement); //? formData.get('sizeSendInfo'); set / append / delete -> could change input after user input
   
   var DATA = [];
   for (const [key, value] of formData) {
@@ -54,10 +37,24 @@ formElement.addEventListener('submit', event => {
   // todo customer_id: user ID/ user login/ user email -> changing if customer needs to be created
   
 
+  var API_HEADERS = new Headers();
+
+  API_HEADERS.append(
+    'Authorization', 'Token token=' + API_TOKEN);
+  API_HEADERS.append(
+    'Content-Type', 'application/json');
+
+
+  var REQUEST_OPTIONS = {
+    method: 'POST',
+    headers: API_HEADERS,
+    body: REQUEST_BODY,
+  };
+
+
   fetch(
     REQUEST_URL,
     REQUEST_OPTIONS,
-    REQUEST_BODY, 
   )
   .then(response => response.text())
   .then(response => console.log(response))
