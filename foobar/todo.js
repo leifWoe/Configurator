9149af6361f3482f0f64ab334ffcace5d51156eb
leifWoe <woehlk.lennart@gmail.com>
2023-08-03 14:00:19 +0200

    clean up

---
+++ foobar/todo.js
@@ -0,0 +1,33 @@
+// todo style.css for when in handy size: first options, rack  pic, output
+
+// todo tests, documentation
+//      delete all unnassary "element" or "options" or alike slang 
+//      principles angewendet: reusability, naming conventions, dry, modularity, single responsibility principle
+
+// todo spam schutz?
+// todo error handling -> try {} catch() {} 
+// const x = condition ? 1 : 2;
+
+// todo query selector for dry principle for same updating behavior
+// todo extra field for special things, controll what can be writen in it?
+// todo komplette preisliste anzeigen können
+
+// todo get.js 
+//      -> https://netbox.intern.speedbone.work/dcim/racks/
+//      -> mysql usage testing with branch from nari
+
+// todo design patterns for how scripts work together
+
+// todo customer infos integrieren (formData.append)
+// todo api_post.js: customer_id: user ID/ user login/ user email -> changing if customer needs to be created
+
+
+//! Update Monthly Total Cost (DRY ATTEMPT) //
+
+// const optionButtons = document.querySelectorAll('.option');
+
+// if (optionButtons) {
+//     optionButtons.addEventListener('click', () => {
+//         updateTotalMonthlyCost();
+//     });
+// };
--- js/index.js
+++ js/index.js
@@ -268,19 +268,6 @@ efMinusOption.addEventListener('click', () => {
 });
 
 
-//! Update Monthly Total Cost (DRY ATTEMPT) //
-
-// const optionButtons = document.querySelectorAll('.option');
-
-// if (optionButtons) {
-//     optionButtons.addEventListener('click', () => {
-//         updateTotalMonthlyCost();
-//     });
-// };
-
-
-
-
 //! SEND BUTTON //
 
 const submitButton = document.getElementById('submitButton');
@@ -291,29 +278,3 @@ if (submitButton) {
         location.href = NEXTPAGE_URL;
     });
 };
-
-
-
-// todo style.css for when in handy size: first options, rack  pic, output
-
-// todo tests, documentation
-//      delete all unnassary "element" or "options" or alike slang 
-//      principles angewendet: reusability, naming conventions, dry, modularity, single responsibility principle
-
-// todo spam schutz?
-// todo error handling -> try {} catch() {} 
-// const x = condition ? 1 : 2;
-
-// todo query selector for dry principle for same updating behavior
-// todo extra field for special things, controll what can be writen in it?
-// todo komplette preisliste anzeigen können
-
-// todo get.js 
-//      -> https://netbox.intern.speedbone.work/dcim/racks/
-//      -> mysql usage testing with branch from nari
-
-// todo design patterns for how scripts work together
-
-// todo customer infos integrieren (formData.append)
-// todo api_post.js: customer_id: user ID/ user login/ user email -> changing if customer needs to be created
-
